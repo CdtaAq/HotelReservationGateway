456acbdedbd758ac901e358e483b1e4c9c196e8b
chris Taylor <ctay456a@gmail.com>
2025-06-09 15:17:44 -0400

    Create UserRepository.java

---
+++ backend/ src/main/java/com/hotel/gateway/Repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.hotel.gateway.repository;
+
+import com.hotel.gateway.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
