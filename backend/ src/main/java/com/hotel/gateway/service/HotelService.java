9ea306112037c455c901595a019b597fe177c3ad
chris Taylor <ctay456a@gmail.com>
2025-06-09 15:19:26 -0400

    Create HotelService.java

---
+++ backend/ src/main/java/com/hotel/gateway/service/HotelService.java
@@ -0,0 +1,11 @@
+package com.hotel.gateway.service;
+
+import com.hotel.gateway.model.Hotel;
+import com.hotel.gateway.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class
