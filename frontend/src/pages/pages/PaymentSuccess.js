7a5eeb16fb12531723e1f0be6c240262b5106ed5
chris Taylor <ctay456a@gmail.com>
2025-06-10 15:42:12 -0400

    Update PaymentSuccess.js

--- frontend/src/pages/pages/PaymentSuccess.js
+++ frontend/src/pages/pages/PaymentSuccess.js
@@ -7,11 +7,4 @@ export default function PaymentSuccess() {
   );
 }
 
-// src/pages/PaymentCancel.js
-export default function PaymentCancel() {
-  return (
-    <div className="p-6 text-center text-red-600 font-bold">
-      Payment was canceled. Please try again.
-    </div>
-  );
-}
+
