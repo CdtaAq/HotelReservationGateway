1c5151a83d471c5866ca4077db3a2b7f229abfbd
chris Taylor <ctay456a@gmail.com>
2025-06-10 15:43:24 -0400

    Create PaymentCancel.js

---
+++ frontend/src/pages/PaymentCancel.js
@@ -0,0 +1,8 @@
+P// src/pages/PaymentCancel.js
+export default function PaymentCancel() {
+  return (
+    <div className="p-6 text-center text-red-600 font-bold">
+      Payment was canceled. Please try again.
+    </div>
+  );
+}
